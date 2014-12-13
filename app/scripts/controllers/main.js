7a412c86232986e1c97e60905d951f6aa0b2eae7
Projjol Banerji <probaner23@gmail.com>
2014-12-13 20:07:42 +0530

    remove works, yay

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,20 @@
 'use strict';
 
+/**
+ * @ngdoc function
+ * @name younoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the younoApp
+ */
 angular.module('younoApp')
   .controller('MainCtrl', function ($scope) {
     $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
-    /*$scope.addTodo = function() {
+    $scope.addTodo = function() {
     	$scope.todos.push($scope.todo);
     	$scope.todo = '';
-    };*/
-    $scope.removeTodo = function(){
-    	$scope.todos.splice(index,1);
+    };
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
     };
   });
