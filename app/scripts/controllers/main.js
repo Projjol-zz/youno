a9bb1fc4ed135320d0a4f088909034cd64d56c1c
Projjol Banerji <probaner23@gmail.com>
2014-12-13 20:02:41 +0530

    stuff has been cookin'

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,13 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name younoApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the younoApp
- */
 angular.module('younoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+    /*$scope.addTodo = function() {
+    	$scope.todos.push($scope.todo);
+    	$scope.todo = '';
+    };*/
+    $scope.removeTodo = function(){
+    	$scope.todos.splice(index,1);
+    };
   });
